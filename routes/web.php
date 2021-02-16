c9434a007884a17a496b452f3f1127c76ef42474
fatim <fatima.rosela@hotmail.com>
2021-02-16 10:03:50 -0700

    clase

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,18 +19,13 @@ Route::get('/', function () {
 });
 
 Route::get('notas', function(){
-    $notas = [
-        /*'Primera Nota',
-        'Segunda Nota',
-        'Tercera Nota',
-        'Cuarta Nota',
-        'Quinta Nota',*/
+    $notas = DB::table('notas')->get();
 
-    ];
+    
 
     return view('notas', ['notas'=> $notas]);
 
-});
+})->name('notas.index');
 
 
 
@@ -37,3 +33,11 @@ Route::get('agregar', function(){
     return view('agregar');
 
 });
+
+Route::get('notas/{id}/editar', function ($id){
+    $notas = DB::table('notas')
+        ->where('id',$id)
+        ->first();
+
+    return 'Aqui se van a editar las notas' .$id;
+})->name('notas.edit');
